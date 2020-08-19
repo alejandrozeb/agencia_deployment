d7ba367631f2726c3769578d72a8194a9e65da50
unknown <alejandrojzeballos@gmail.com>
2020-08-19 00:00:02 -0400

    mostrando el port y el host

--- server/index.js
+++ server/index.js
@@ -47,5 +47,7 @@ app.use('/',routes());
 const host=process.env.HOST || '0.0.0.0';
 const port=process.env.PORT || 3000;
 app.listen(port,host, () => {
+    console.log(port);
+    console.log(host);
     console.log('el servidor esta funcionando');
 });
